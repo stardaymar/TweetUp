a7b36e28c9c6415a4d97387ee0d96a3e9bb3256d
Daymar <dayrosaa99@gmail.com>
2023-04-24 00:21:38 -0400

    Inicializando vite y creacion de componentes

---
+++ src/components/Form.jsx
@@ -0,0 +1,113 @@
+import React, { useState, useEffect } from 'react';
+import AlertError from './AlertError';
+
+const Form = ({ tweet, tweets, setTweets, setTweet }) => {
+  const [descripcion, setDescripcion] = useState('');
+  const [fecha, setFecha] = useState('');
+
+  const [error, setError] = useState(false);
+
+  useEffect(() => {
+    if (Object.keys(tweet).length > 0) {
+      setDescripcion(tweet.descripcion);
+      setFecha(tweet.fecha);
+    }
+  }, [tweet]);
+
+  const generarId = () => {
+    const id = Math.random().toString(20).substr(2);
+    return id;
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if ([descripcion, fecha].includes('')) {
+      setError(true);
+      return;
+    }
+    setError(false);
+
+    // objeto de tweets
+    const objetoTweets = {
+      descripcion,
+      fecha,
+    };
+
+    // Editar tweet
+    if (tweet.id) {
+      objetoTweets.id = tweet.id;
+
+      const tweetsActualizados = tweets.map((tweetState) =>
+        tweetState.id === tweet.id ? objetoTweets : 'una cosa'
+      );
+      setTweets(tweetsActualizados);
+      setTweet({});
+    } else {
+      // Spread operator ...tareas
+      objetoTweets.id = generarId();
+      setTweets([...tweets, objetoTweets]);
+    }
+
+    // limpiar formulario
+    setDescripcion('');
+    setFecha('');
+  };
+
+  return (
+    <div className=''>
+      <div className='shadow-lg bg-slate-200 rounded-md mx-6 py-3'>
+        <form onSubmit={handleSubmit}>
+          {error && (
+            <AlertError>Falta diligenciar campos del formulario</AlertError>
+          )}
+
+          <div className='auto-rows-auto m-4'>
+            <label className='w-full'>✨ Fecha</label>
+            <input
+              className='rounded p-3 shadow-lg w-full'
+              type='date'
+              name=''
+              id=''
+              placeholder='Selecciona la fecha'
+              value={fecha}
+              onChange={(e) => setFecha(e.target.value)}
+            />
+          </div>
+          <div className='auto-rows-auto m-4'>
+            <label className='w-full'>📱 Descripción</label>
+            <input
+              className='rounded p-3 shadow-lg w-full'
+              type='text'
+              name=''
+              id=''
+              placeholder='Qué estas pensando?'
+              value={descripcion}
+              onChange={(e) => setDescripcion(e.target.value)}
+            />
+          </div>
+
+          {tweet.id ? (
+            <div className='auto-rows-auto m-4'>
+              <input
+                className='w-full bg-purple-600 p-3 rounded-full text-white font-bold hover:bg-purple-900 cursor-pointer uppercase'
+                type='submit'
+                value='Actualizar'
+              />
+            </div>
+          ) : (
+            <div className='auto-rows-auto m-4'>
+              <input
+                className='w-full bg-blue-600 p-3 rounded-full text-white font-bold hover:bg-blue-900 cursor-pointer uppercase'
+                type='submit'
+                value='Subir tweet'
+              />
+            </div>
+          )}
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default Form;
---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../index.css';
+
+const Header = () => {
+  return (
+    <div className='font-black text-gray-300 text-center content-center text-3xl my-10'>
+      Listado de <span className='text-green-400'>tweets</span>
+    </div>
+  );
+};
+
+export default Header;
